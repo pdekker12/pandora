13b2b2643520a802f9336b8e22d2feb18c3bc793
Thibault Clérice <leponteineptique@gmail.com>
2017-09-24 15:08:53 +0200

    Clean up of the CLI. Fixed #21

--- main.py
+++ main.py
@@ -7,7 +7,25 @@ import pandora.utils
 from pandora.tagger import Tagger
 
 
-def main(config, train='data/wilhelmus/all_train', dev='data/wilhelmus/all_dev', test='data/wilhelmus/all_test', load=False, **kwargs):
+def main(config, train, dev, test=None, load=False, **kwargs):
+    """ Main CLI Interface
+
+    :param config: Path to retrieve configuration file
+    :type config: str
+    :param train: Path to directory containing dev files
+    :type train: str
+    :param dev: Path to directory containing test files
+    :type dev: str
+    :param test: Path to directory containing train files
+    :type test: str
+    :param load: Whether to load an existing model to train on top of it (default: False
+    :type load: bool
+    :param nb_epochs: Number of epoch
+    :type nb_epochs: int
+    :param kwargs: Other arguments
+    :type kwargs: dict
+    :return:
+    """
     print('::: started :::')
     params = pandora.utils.get_param_dict(config)
     params['config_path'] = config
@@ -35,41 +53,39 @@ def main(config, train='data/wilhelmus/all_train', dev='data/wilhelmus/all_dev',
         include_morph=params['include_morph'],
         nb_instances=None
     )
-    
-    test_data = pandora.utils.load_annotated_dir(
-        test,
-        format='tab',
-        extension='.tab',
-        include_pos=params['include_pos'],
-        include_lemma=params['include_lemma'],
-        include_morph=params['include_morph'],
-        nb_instances=None
+
+    data_sets = dict(
+            train_data=train_data,
+            dev_data=dev_data
     )
-    
+
+    if test is not None:
+        test_data = pandora.utils.load_annotated_dir(
+            test,
+            format='tab',
+            extension='.tab',
+            include_pos=params['include_pos'],
+            include_lemma=params['include_lemma'],
+            include_morph=params['include_morph'],
+            nb_instances=None
+        )
+        data_sets["test_data"] = test_data
+
     if load:
         print('::: loading model :::')
         tagger = Tagger(load=True, model_dir=params['model_dir'])
-        tagger.setup_to_train(
-            train_data=train_data,
-            dev_data=dev_data,
-            test_data=test_data,
-            build=False
-        )
+        tagger.setup_to_train(build=False, **kwargs)
         tagger.curr_nb_epochs = int(params['curr_nb_epochs'])
-        print("restart from epoch ", str(tagger.curr_nb_epochs), "...")
+        print("restart from epoch "+str(tagger.curr_nb_epochs)+"...")
         tagger.setup = True
     else:
         tagger = Tagger(**params)
-        tagger.setup_to_train(
-            train_data=train_data,
-            dev_data=dev_data,
-            test_data=test_data
-        )
+        tagger.setup_to_train(**data_sets)
     
     for i in range(int(params['nb_epochs'])):
-        tagger.epoch()
-        tagger.test()
-        tagger.save()
+        tagger.epoch(autosave=True)
+        if test is not None:
+            tagger.test()
 
     tagger.save()
     print('::: ended :::')
@@ -87,7 +103,7 @@ if __name__ == '__main__':
         dest="load",
         action="store_true", 
         default=False,
-        help="wether to load an existing model to train on top of it (default: False)"
+        help="Whether to load an existing model to train on top of it (default: False)"
     )
 
     main(**vars(parser.parse_args()))
