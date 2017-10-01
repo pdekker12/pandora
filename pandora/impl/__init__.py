8f71dbca8d9d5eb335eb43ed9fb17705d65b5113
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-10-01 21:26:07 +0200

    import pytorch before keras to avoid wierd importerror

--- pandora/impl/__init__.py
+++ pandora/impl/__init__.py
@@ -1,10 +1,10 @@
 try:
-    from pandora.impl.keras.model import KerasModel
-except ImportError:
-    print("Couldn't import Keras. Keras implementation not available")
-    KerasModel = None
-try:
     from pandora.impl.pytorch.model import PyTorchModel
 except ImportError:
     print("Couldn't import PyTorch. PyTorch implementation not available")
     PyTorchModel = None
+try:
+    from pandora.impl.keras.model import KerasModel
+except ImportError:
+    print("Couldn't import Keras. Keras implementation not available")
+    KerasModel = None
