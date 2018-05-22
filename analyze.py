757fc811f22085140ed18e4db90916d8ce4ee60a
Peter Dekker <peter@peterdekker.eu>
2018-05-22 15:53:33 +0200

    Improved analysis of errors for both POS tagging and lemmatization

--- analyze.py
+++ analyze.py
@@ -6,53 +6,59 @@ import argparse
 import pandas as pd
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--tagged_file", default="output/test_corpus.txt.tsv")
-parser.add_argument("--gold_file", default="/home/peter/box/POS-tagging/data/BrievenAlsBuit/test10000/test_corpus.tsv")
-parser.add_argument("--gold_col_tokens", type=int, default=0)
-parser.add_argument("--gold_col_pos", type=int, default=1)
-parser.add_argument("--gold_col_lemma", type=int, default=2)
 
+def error_analysis(df_comb, var, var_gold):
+    # Analyze wrong lemma results
+    df_wrong = df_comb[df_comb[var]!=df_comb[var_gold]]
+    df_wrong = df_wrong[["tokens", var, var_gold]]
+    # Sort by most frequent combination
+    return df_wrong.groupby(["tokens", var, var_gold]).size().sort_values(ascending=False)
+    
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tagged_file", default="output/test_corpus.txt.tsv")
+    parser.add_argument("--gold_file", default="/home/peter/box/POS-tagging/data/BrievenAlsBuit/test10000/test_corpus.tsv")
+    parser.add_argument("--gold_col_tokens", type=int, default=0)
+    parser.add_argument("--gold_col_pos", type=int, default=1)
+    parser.add_argument("--gold_col_lemma", type=int, default=2)
+    
+    
+    args = parser.parse_args()
+    
+    # Read tagged file
+    df_tagged = pd.read_csv(args.tagged_file, sep="\t", header=0)
+    df_tagged = df_tagged.drop(columns=["lemmas"])
+    df_tagged = df_tagged.rename(columns={"postcorrect_lemmas":"lemmas"})
+    # Read gold file
+    # Match tab or multiple whitespaces as delimiter
+    df_gold = pd.read_csv(args.gold_file, sep="\s{2,}|\t+", engine="python", header=None)
+    df_gold = df_gold.rename(columns={df_gold.columns[args.gold_col_tokens]: "tokens", df_gold.columns[args.gold_col_pos]: "pos_gold", df_gold.columns[args.gold_col_lemma]: "lemmas_gold" })
+    
+    # Check if gold and tagged file contain same words
+    if not df_tagged["tokens"].equals(df_gold["tokens"]):
+        raise IOError("Gold and tagged file do not contain same tokens!")
+    
+    # Combine dataframes
+    df_comb = pd.concat([df_tagged,df_gold],axis=1)
+    df_comb = df_comb.loc[:,~df_comb.columns.duplicated()]
+    
+    # Compute lemmatization and POS tagging accuracy
+    correct_lemma_count = len(df_comb[df_comb["lemmas"]==df_comb["lemmas_gold"]])
+    correct_pos_count = len(df_comb[df_comb["pos"]==df_comb["pos_gold"]])
+    correct_both_count = len(df_comb[(df_comb["pos"]==df_comb["pos_gold"]) & (df_comb["lemmas"]==df_comb["lemmas_gold"])])
+    total = len(df_comb)
+    acc_lemma = correct_lemma_count / total
+    acc_pos = correct_pos_count / total
+    acc_both = correct_both_count / total
+    print("Lemmatization accuracy: " + str(acc_lemma))
+    print("POS tagging accuracy: " + str(acc_pos))
+    print("POS&lemma accuracy: " + str(acc_both) + ". Expected (pos*lemma): " + str(acc_lemma*acc_pos))
+    
+    # Show analysis of most frequent errors
+    print(error_analysis(df_comb,"lemmas", "lemmas_gold"))
+    print(error_analysis(df_comb,"pos", "pos_gold"))
 
-# Read tagged file
-df_tagged = pd.read_csv(args.tagged_file, sep="\t", header=0)
-df_tagged = df_tagged.drop(columns=["lemmas"])
-df_tagged = df_tagged.rename(columns={"postcorrect_lemmas":"lemmas"})
-# Read gold file
-# Match tab or multiple whitespaces as delimiter
-df_gold = pd.read_csv(args.gold_file, sep="\s{2,}|\t+", engine="python", header=None)
-df_gold = df_gold.rename(columns={df_gold.columns[args.gold_col_tokens]: "tokens", df_gold.columns[args.gold_col_pos]: "pos_gold", df_gold.columns[args.gold_col_lemma]: "lemmas_gold" })
+if __name__ == "__main__":
+    main()
 
-# Check if gold and tagged file contain same words
-if not df_tagged["tokens"].equals(df_gold["tokens"]):
-    raise IOError("Gold and tagged file do not contain same tokens!")
-
-# Combine dataframes
-df_comb = pd.concat([df_tagged,df_gold],axis=1)
-df_comb = df_comb.loc[:,~df_comb.columns.duplicated()]
-
-# Compute lemmatization and POS tagging accuracy
-correct_lemma_count = len(df_comb[df_comb["lemmas"]==df_comb["lemmas_gold"]])
-correct_pos_count = len(df_comb[df_comb["pos"]==df_comb["pos_gold"]])
-correct_both_count = len(df_comb[(df_comb["pos"]==df_comb["pos_gold"]) & (df_comb["lemmas"]==df_comb["lemmas_gold"])])
-total = len(df_comb)
-acc_lemma = correct_lemma_count / total
-acc_pos = correct_pos_count / total
-acc_both = correct_both_count / total
-print("Lemmatization accuracy: " + str(acc_lemma))
-print("POS tagging accuracy: " + str(acc_pos))
-print("POS&lemma accuracy: " + str(acc_both) + ". Expected (pos*lemma): " + str(acc_lemma*acc_pos))
-
-
-# Analyze wrong lemma results
-df_wrong_lemma = df_comb[df_comb["lemmas"]!=df_comb["lemmas_gold"]]
-df_wrong_lemma = df_wrong_lemma[["tokens", "lemmas", "lemmas_gold"]]
-# Sort by most frequent combination
-print(df_wrong_lemma.groupby(['lemmas', 'lemmas_gold']).size().sort_values(ascending=False))
-
-# Analyze wrong POS results
-df_wrong_pos = df_comb[df_comb["pos"]!=df_comb["pos_gold"]]
-df_wrong_pos = df_wrong_pos[["tokens", "pos", "pos_gold"]]
-print(df_wrong_pos)
